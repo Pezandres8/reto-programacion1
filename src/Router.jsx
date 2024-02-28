41bc019eb9c25f14fd8bc1bf36010debcf0e2654
Cristian Betancourt <cristian.camilo.betancourt.rin@gmail.com>
2024-02-27 19:42:37 -0500

    Home page & tailwind integration

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,16 @@
 import { Route, Switch } from "wouter";
 import { Home } from "./pages/Home/Home";
+import { Detail } from "./pages/Detail/Detail";
 
 function Router() {
-
   return (
     <>
       <Switch>
-        <Route path="/">
-        {<Home/>}
-        </Route>
+        <Route path="/">{<Home />}</Route>
+        <Route path="/detalle-produicto/:id">{<Detail />}</Route>
       </Switch>
     </>
-  )
+  );
 }
 
-export default Router; 
+export default Router;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { Card } from "../../components/card/Card";
 
 export const Home = () => {
-    const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
       .then((res) => res.json())
       .then((responseParse) => {
-        console.log(responseParse)
-         setRecipes(responseParse.categories)
- 
+        console.log(responseParse);
+        setRecipes(responseParse.meals);
       })
-      .catch((err) => console.error(err))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <>
-      <div>Home</div>
-      {recipes.map((recipes) =>{
-          
-      })}
+      <h1>Home</h1>
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            {recipes.map((recipes) => {
+              return (
+                <>
+                  <Card
+                    dishName={recipes.strMeal}
+                    dishId={recipes.idMeal}
+                    dishImage={recipes.strMealThumb}
+                  />
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
